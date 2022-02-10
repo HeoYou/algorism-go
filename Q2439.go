f9e133469fbc2f2546685d68a027e3dd60d1b483
HeoYou <lov21y_@naver.com>
2022-02-10 23:30:22 +0900

    아니 왜 내껀 안되는거야 모르겠어...

--- Q2439.go
+++ Q2439.go
@@ -1,19 +1,47 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var a int
+// 	star := "*"
+// 	fmt.Scanln(&a)
+
+// 	for i := a; i > 0; i-- {
+
+// 		for j := 1; j < i; j++ {
+// 			fmt.Print(" ")
+// 		}
+
+// 		fmt.Println(star)
+// 		star += "*"
+// 	}
+// }
+
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+
 	var a int
-	star := "*"
-	fmt.Scanln(&a)
+	fmt.Fscanln(r, &a)
+	fmt.Println(a)
 
 	for i := a; i > 0; i-- {
-
-		for j := 1; j < i; j++ {
-			fmt.Print(" ")
+		for j := 0; j < i; j++ {
+			fmt.Fprintf(w, " ")
+			fmt.Print("123")
 		}
-
-		fmt.Println(star)
-		star += "*"
+		for j := 0; j < i; j++ {
+			fmt.Fprintf(w, "*")
+		}
 	}
 }
