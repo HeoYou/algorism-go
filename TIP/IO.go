9f734689e33702deeb6f101c45e82f8a03555c6e
HeoYou <lov21y_@naver.com>
2022-02-16 01:04:58 +0900

    buffer랑 둘다 해보았다.  속도차이가 어마무시하다

--- TIP/IO.go
+++ TIP/IO.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"time"
 )
 
+// func main() {
+// 	start := time.Now()
+// 	for i := 0; i < 50000; i++ {
+// 		fmt.Print("abc")
+// 	}
+
+// 	end := time.Now()
+// 	fmt.Println(end.Sub(start))
+// 	//5.8s ln
+// 	//1.49s
+// }
+
 func main() {
 	start := time.Now()
+	w := bufio.NewWriter(os.Stdout)
 	for i := 0; i < 50000; i++ {
-		fmt.Println("abc")
+		fmt.Fprintln(w, "a")
 	}
 
 	end := time.Now()
 	fmt.Println(end.Sub(start))
+	//ln 39.7799ms
 }
