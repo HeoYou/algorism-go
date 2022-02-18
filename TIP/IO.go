374624f1eb0649c441255dc8285a6c1b0c51cb4d
HeoYou <lov21y_@naver.com>
2022-02-18 21:17:35 +0900

    input 정리

--- TIP/IO.go
+++ TIP/IO.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"time"
 )
 
+// output no buffer
 // func main() {
 // 	start := time.Now()
 // 	for i := 0; i < 50000; i++ {
@@ -19,14 +19,33 @@ import (
 // 	//1.49s
 // }
 
-func main() {
-	start := time.Now()
-	w := bufio.NewWriter(os.Stdout)
-	for i := 0; i < 50000; i++ {
-		fmt.Fprintln(w, "a")
-	}
+// output use buffer
+// func main() {
+// 	start := time.Now()
+// 	w := bufio.NewWriter(os.Stdout)
+// 	for i := 0; i < 50000; i++ {
+// 		fmt.Fprintln(w, "a")
+// 	}
 
-	end := time.Now()
-	fmt.Println(end.Sub(start))
-	//ln 39.7799ms
+// 	end := time.Now()
+// 	fmt.Println(end.Sub(start))
+// 	//39.7799ms
+// 	//3.41s
+// }
+
+// bufio 사용이 월등히 빠른걸 알 수 있음
+
+//input use buffer 공백이 있으면 맨 앞 인풋만 들어감
+func main() {
+	var num int
+	r := bufio.NewReader(os.Stdin)
+	fmt.Fscan(r, &num)
+	fmt.Println(num)
 }
+
+//input use buffer 한줄 읽기
+// func main() {
+// 	r := bufio.NewReader(os.Stdin)
+// 	s, _ := r.ReadString('\n')
+// 	fmt.Print(s)
+// }
